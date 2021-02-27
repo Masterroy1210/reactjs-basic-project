49a92b53fbdab152d008be15dd8805c0d897f63b
Masterroy1210 <67965579+Masterroy1210@users.noreply.github.com>
2021-02-27 16:14:31 +0530

    Add files via upload
    This is a basic ui developed in react and bootstrap which retrives data from a fake javascript apii and show it in the tabular form on the webpage and also it hs  delete button 
    this  is just a basic ui develeoped in react to understand the render,conditional rendering,javascript basic,jsx and server

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Movie from './components/movies';
+import './App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (  <main className = "Container">
+      <Movie />
+    
+  </main> );
+  }
+}
+ 
+ 
+
+export default App;
